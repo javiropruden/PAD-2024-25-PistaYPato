95744d6344310d1cf4490fa0688ab2f1b3b86de2
Naive <congchanghong@gmail.com>
2024-11-12 04:50:34 +0100

    interfaz iniciaSesion

---
+++ app/src/main/java/es/ucm/fdi/pistaypato/iniciaSesion.java
@@ -0,0 +1,13 @@
+package es.ucm.fdi.pistaypato;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class iniciaSesion extends AppCompatActivity {
+
+}
