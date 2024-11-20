8a6725a99adf00e3aba084b5338b6ae20ce816c0
javiropruden <114072973+javiropruden@users.noreply.github.com>
2024-11-20 14:16:49 +0100

    Primera Implementación perfil y modificarPerfil

--- app/src/main/java/es/ucm/fdi/pistaypato/ModificarPerfilActivity.java
+++ app/src/main/java/es/ucm/fdi/pistaypato/ModificarPerfilActivity.java
@@ -13,8 +13,14 @@ public class ModificarPerfilActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Habilita la funcionalidad EdgeToEdge para ajustar los insets del sistema
         EdgeToEdge.enable(this);
+
+        // Asocia la vista con el layout de activity_modificar_perfil
         setContentView(R.layout.activity_modificar_perfil);
+
+        // Ajusta automáticamente los paddings para evitar superposiciones con barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/es/ucm/fdi/pistaypato/PerfilActivity.java
+++ app/src/main/java/es/ucm/fdi/pistaypato/PerfilActivity.java
@@ -13,11 +13,20 @@ public class PerfilActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Activa EdgeToEdge para manejar el espacio en pantalla correctamente
         EdgeToEdge.enable(this);
+
+        // Asocia la vista al layout de activity_perfil
         setContentView(R.layout.activity_perfil);
+
+        // Ajusta automáticamente los paddings de la vista principal para los insets del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+            // Ajusta el padding de la vista para acomodar las barras de sistema (superior e inferior)
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
             return insets;
         });
     }
