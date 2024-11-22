7847683153779073bdcb31d462778558ac10eaf0
javiropruden <114072973+javiropruden@users.noreply.github.com>
2024-11-22 15:18:56 +0100

    Modificar Perfil, Password y perfil

---
+++ app/src/main/java/es/ucm/fdi/pistaypato/PasswordFragment.java
@@ -0,0 +1,83 @@
+package es.ucm.fdi.pistaypato;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PasswordFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PasswordFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private View view;
+    private Button continuar;
+    private EditText password;
+    private EditText repeatPassword;
+
+    public PasswordFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Password.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PasswordFragment newInstance(String param1, String param2) {
+        PasswordFragment fragment = new PasswordFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        this.view = inflater.inflate(R.layout.fragment_perfil, container, false);
+        this.continuar = view.findViewById(R.id.password_continuar);
+        this.password = view.findViewById(R.id.password_password);
+        this.repeatPassword = view.findViewById(R.id.password_repeat_password);
+
+        this.continuar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                inflater.inflate(R.layout.fragment_modificar_perfil, container, false);
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_password, container, false);
+    }
+}
