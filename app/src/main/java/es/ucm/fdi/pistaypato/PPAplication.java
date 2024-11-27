277f336bb551f12a82ce899e9268db3b588a08db
mikel0313 <mikalvar@ucm.es>
2024-11-27 18:06:47 +0100

    Commit avances bd

--- app/src/main/java/es/ucm/fdi/pistaypato/PPAplication.java
+++ app/src/main/java/es/ucm/fdi/pistaypato/PPAplication.java
@@ -2,25 +2,55 @@ package es.ucm.fdi.pistaypato;
 
 import android.app.Application;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import org.json.JSONArray;
 
 import java.util.ArrayList;
 
 public class PPAplication extends Application {
+
+    // Almacena los campos de bádminton
     public ArrayList<String> badmintonFields;
-    JSONArray jsonArray;
+    public JSONArray jsonArray;
+
+    // Referencias para Firebase
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference usersReference;
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Inicializa la lista
         badmintonFields = new ArrayList<>();
         jsonArray = new JSONArray();
-        //la base de datos se deberia inicializar aqui
-        //FirebaseApp.initializeApp(this);
+
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        usersReference = firebaseDatabase.getReference("users");
+
+
     }
 
-    public ArrayList<String> getFiels(){
+    // Método para agregar un nuevo usuario a Firebase Realtime Database
+    public void addUser(String userId, String firstName, String lastName, String email, String password) {
+        User newUser = new User(userId, firstName, lastName, email, password);
+        usersReference.child(userId).setValue(newUser)
+                .addOnSuccessListener(aVoid -> {
+                    System.out.println("Usuario añadido correctamente");
+                })
+                .addOnFailureListener(e -> {
+                    System.err.println("Error al añadir usuario: " + e.getMessage());
+                });
+    }
+
+    // Método para obtener la lista de campos de bádminton
+    public ArrayList<String> getFields() {
         return badmintonFields;
     }
-}
 
+}
